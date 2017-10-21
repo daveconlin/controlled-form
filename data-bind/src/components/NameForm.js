28f36f4cddd2a6ba2afe83d11f5fcfd02359fa39
Dave Conlin <daveconlin@mac.com>
2017-10-20 22:45:14 -0400

    edits to form submit action

--- data-bind/src/components/NameForm.js
+++ data-bind/src/components/NameForm.js
@@ -1,57 +1,98 @@
-import React from 'react'
-import MaskedInput from 'react-text-mask';
-
+import React from "react";
+import MaskedInput from "react-text-mask";
 
 class NameForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            first: "",
-            last: "",
-            ssn: "",
-            error: null
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: "",
+      last: "",
+      ssn: "",
+      error: null,
+      scrubFirst: "Victor",
+      scrubLast: "Aponte",
+      scrubSSN: "090-444-2245"
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({
-            [event.target.id]: event.target.value,
-        });
-    }
+  handleChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
 
-    handleSubmit(event) {
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      scrubFirst: this.state.first,
+      scrubLast: this.state.last,
+      scrubSSN: this.state.ssn
+    });
+  }
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <p>{this.state.first} {this.state.last}</p>
-                <p>{this.state.ssn}</p>
-                <p>
-                    <label htmlFor="ssn">Social Security:</label>
-                    {/*<input type="text" value={this.state.value} onChange={this.handleChange} />*/}
-                    <MaskedInput
-                        mask={[/\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-                        className="form-control"
-                        placeholder="Enter SSN"
-                        guide={false}
-                        id="ssn"
-                        onChange={this.handleChange}
-                        value={this.state.ssn}
-                    />
-                    <label htmlFor="first">First Name:</label>
-                    <input type="text" id="first" value={this.state.first} onChange={this.handleChange}/>
-                    <label htmlFor="last">Last Name:</label>
-                    <input type="text" id="last" value={this.state.last} onChange={this.handleChange}/>
-                    <input type="submit" value="Submit"/>
-                </p>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <hr />
+        <p>
+          {this.state.scrubFirst} {this.state.scrubLast}
+        </p>
+        <p>{this.state.scrubSSN}</p>
+        <hr />
+        <p>
+          <label htmlFor="ssn">Social Security:</label>
+          {/*<input type="text" value={this.state.value} onChange={this.handleChange} />*/}
+          <MaskedInput
+            mask={[
+              /\d/,
+              /\d/,
+              /\d/,
+              "-",
+              /\d/,
+              /\d/,
+              "-",
+              /\d/,
+              /\d/,
+              /\d/,
+              /\d/
+            ]}
+            className="form-control"
+            placeholder="Enter SSN"
+            guide={false}
+            id="ssn"
+            onChange={this.handleChange}
+            value={this.state.ssn}
+          />
+        </p>
+        <p>
+          {" "}
+          <label htmlFor="first">First Name:</label>
+          <input
+            type="text"
+            id="first"
+            placeholder="Enter First Name"
+            value={this.state.first}
+            onChange={this.handleChange}
+          />
+        </p>
+        <p>
+          <label htmlFor="last">Last Name:</label>
+          <input
+            type="text"
+            placeholder="Enter Last Name"
+            id="last"
+            value={this.state.last}
+            onChange={this.handleChange}
+          />
+        </p>
+        <hr />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 export default NameForm;
